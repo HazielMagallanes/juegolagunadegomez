397f0064d9bbdc5c21e5b57164606dbc6b9e64bd
Haziel Magallanes <hazielescuela1@gmail.com>
2025-05-15 14:25:49 -0300

    Hidden clock UI

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,4 +1,4 @@
-extends Window
+extends Node
 
 @onready var musicPlayer = $MusicPlayer;
 @onready var clockLayer = $Clock;
@@ -6,17 +6,10 @@ extends Window
 @onready var clock = $TimeVFX;
 func _ready() -> void:
 	SignalBus.connect("changeLevel", Callable(self, "on_changeLevel"));
-	clockLayer.visible = true;
-	clock.visible = true;
-	SignalBus.timeTick.connect(clockUI.setDaytime);
-
 func on_changeLevel(path: String, spawn: Vector2):
-	clock.visible = true;
 	$Level.remove_child($Level/Level)
 	$Level.add_child(load(path).instantiate());
 	if $Level/Level.has_node("Player"):
 		$Level/Level/Player.global_position = spawn;
-	if $Level/Level.get_meta("isInterior", false):
-		clock.visible = false;
 func _process(delta: float) -> void:
 	if !musicPlayer.playing: musicPlayer.play();
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,5 +2,4 @@ extends Control
 
 func _ready() -> void:
 	get_tree().paused = true;
-	get_parent().get_parent().get_node("./Clock/DayNightCycleUI").visible = false;
 	get_parent().get_parent().get_node("./TimeVFX").visible = false;
